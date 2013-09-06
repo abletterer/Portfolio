40b82b5e7c48a2d12647ada24ebc356568c61e7d
107509 <u74601854@false.(none)>
2013-09-06 10:27:38 +0200

    Modifications

--- identifiants.php
+++ identifiants.php
@@ -1,6 +1,6 @@
 <?php 
-$stringConn = 'mysql:host=localhost;dbname=portfolio';
-$userConn = 'root';
-$mdpConn = 'tarask';
+$stringConn = 'mysql:host=db489221540.db.1and1.com;dbname=db489221540';
+$userConn = 'dbo489221540';
+$mdpConn = 'tarask67';
 $argsConn = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 ?>
