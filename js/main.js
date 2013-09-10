8344382045ef8787646b07eaf7f5b5f7a6d8be43
Arnaud Bletterer <arnaud.bletterer@gmail.com>
2013-09-10 08:48:14 +0200

    Modification présentation header

--- js/main.js
+++ js/main.js
@@ -2,28 +2,24 @@ $(function() {
 	$('.fleche-bas').transition({ rotate: '90deg', duration:0});
 	
 	$('.title-h1-activable').next().children().hide();
-	
-	$('.fleche-bas').click(function() {
-		$(this).parent().parent().next().children().fadeToggle();
-		if($(this).next().attr("value")=="inactive") {
-			$(this).transition({ rotate: '0deg' });
-			$(this).next().attr("value","active");
-		}
+    
+    $('.title-h1-activable-first').click(function() {
+		$(this).next().children().fadeToggle();
+		if($(this).children("input").attr("value")=="inactive") {
+			$(this).children(".fleche-bas").transition({ rotate: '0deg' });
+			$(this).children("input").attr("value","active");
+		} 
 		else {
-			$(this).transition({ rotate: '90deg' });
-			$(this).next().attr("value","inactive");
+			$(this).children(".fleche-bas").transition({ rotate: '90deg' });
+			$(this).children("input").attr("value","inactive");
 		}
 	});
     
-    $('.fleche-bas-first').click(function() {
-		$(this).parent().parent().next().children().fadeToggle();
-		if($(this).next().attr("value")=="inactive") {
-			$(this).transition({ rotate: '0deg' });
-			$(this).next().attr("value","active");
-		}
-		else {
-			$(this).transition({ rotate: '90deg' });
-			$(this).next().attr("value","inactive");
-		}
-	});
+    $(".social>a>img").hover(function() {
+        $(this).transition({scale: 2});
+    });
+    
+    $(".social>a>img").mouseout(function() {
+        $(this).transition({scale: 1/2});
+    });
 });
